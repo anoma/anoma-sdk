c25a8895453122332a2b7dec387267bb93cbae7a
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-06-18 18:30:03 +0200

    wip commit

--- lib/arm/arm.ex
+++ lib/arm/arm.ex
@@ -6,9 +6,9 @@ defmodule Anoma.Arm do
     otp_app: :anoma_sdk,
     crate: :zkvm
 
-
   def test_delta_witness, do: :erlang.nif_error(:nif_not_loaded)
   def test_delta_witness(_delta_witness), do: :erlang.nif_error(:nif_not_loaded)
 
-
+  # def test_delta_proof, do: :erlang.nif_error(:nif_not_loaded)
+  def test_delta_proof(_delta_proof), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/zkvm/src/lib.rs
+++ native/zkvm/src/lib.rs
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------//
-//                                Functions                                   //
+//                                Delta Witness                               //
 //----------------------------------------------------------------------------//
 
 use aarm_core::delta_proof::{DeltaWitness};
@@ -20,6 +20,30 @@ fn test_delta_witness() -> DeltaWitness {
 #[rustler::nif]
 fn test_delta_witness(delta_witness: DeltaWitness) -> DeltaWitness {
     delta_witness
+}
+
+//----------------------------------------------------------------------------//
+//                                Delta Proof                               //
+//----------------------------------------------------------------------------//
+
+use aarm_core::delta_proof::{DeltaProof};
+// #[rustler::nif]
+// fn test_delta_witness() -> DeltaProof {
+//     // // create a random delta witness
+//     // use k256::ecdsa::SigningKey;
+//     // use k256::elliptic_curve::rand_core::OsRng;
+//     //
+//     // let mut rng = OsRng;
+//     // let signing_key = SigningKey::random(&mut rng);
+//     //
+//     // DeltaWitness {
+//     //     signing_key: signing_key,
+//     // }
+// }
+
+#[rustler::nif]
+fn test_delta_proof(delta_proof: DeltaProof) -> DeltaProof {
+    delta_proof
 }
 
 rustler::init!("Elixir.Anoma.Arm");
