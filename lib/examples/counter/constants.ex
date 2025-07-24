9a6804e950fd85275d290ec7cc1fe4f5356fd413
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-07-24 16:42:27 +0200

    wip commit

---
+++ lib/examples/counter/constants.ex
@@ -0,0 +1,5 @@
+defmodule Anoma.Examples.Counter.Constants do
+  @moduledoc """
+  I define constants being used in the Counter example.
+  """
+end
---
+++ lib/examples/counter/counter.ex
@@ -0,0 +1,10 @@
+defmodule Anoma.Examples.Counter do
+  @moduledoc """
+  I define an example application to work with counters as resources.
+  """
+    # use Rustler,
+    # otp_app: :anoma_sdk,
+    # crate: :counter_example
+
+
+end
