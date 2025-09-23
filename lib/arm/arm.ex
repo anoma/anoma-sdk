94c8a5cdefcafe7bfd067d75c3a5636cb4e567eb
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-09-23 20:09:41 +0200

    remove comment

--- lib/arm/arm.ex
+++ lib/arm/arm.ex
@@ -14,12 +14,6 @@ defmodule AnomaSDK.Arm do
     version: "0.0.0",
     force_build: System.get_env("BUILD_NATIVE") in ["1", "true"]
 
-  # targets: [
-  #   "aarch64-apple-darwin",
-  #   "x86_64-apple-darwin",
-  #   "x86_64-unknown-linux-gnu"
-  # ]
-
   alias AnomaSDK.Arm.ComplianceInstance
   alias AnomaSDK.Arm.ComplianceUnit
   alias AnomaSDK.Arm.ComplianceWitness
