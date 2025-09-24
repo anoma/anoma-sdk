005852420d83685b3a05753af0079379a09241c2
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-09-24 11:05:39 +0200

    remove checksums

--- checksum-Elixir.AnomaSDK.Arm.exs
+++
@@ -1,5 +0,0 @@
-%{
-  "libarm_bindings-v0.0.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:0058b6aadc6f9a0b1528e4a05d0abb311b7b36d0123459b68eb8dc5f9f323ba4",
-  "libarm_bindings-v0.0.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:3c6ace5cc168e42dda5a672fc5a47f3170c580af1b5295dbb18ff02cb454202f",
-  "libarm_bindings-v0.0.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d485fe18de3205143c0d99d00e92ca7e4be20e90a638a71891f472b357d1435e",
-}
